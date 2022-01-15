529f48e4d38266d54731b217ecbccdea2cbbb074
brusdamson <brusdamson@gmail.com>
2022-01-15 20:15:55 +0300

    Начало тестовой ветки

--- CsGoMarketBot/Program.cs
+++ CsGoMarketBot/Program.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using CsGoMarketBot.MarketMaster;
 namespace CsGoMarketBot
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
+            IResponseMarket market = new ResponseMarket("");
         }
     }
 }
