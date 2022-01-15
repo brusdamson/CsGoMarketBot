e5146436912d50ef44223be9c09a03bf54f6a6e1
brusdamson <brusdamson@gmail.com>
2022-01-15 21:50:48 +0300

    Переделка запросов

--- CsGoMarketBot/MarketMaster/ResponseMarket.cs
+++ CsGoMarketBot/MarketMaster/ResponseMarket.cs
@@ -16,43 +16,6 @@ namespace CsGoMarketBot.MarketMaster
             _marketSecret = marketSecret;
         }
         
-        /// <summary>
-        /// [id] — id предмета.
-        /// [price] — цена в копейках (1 RUB =100, 1 USD = 1000, 1 EUR = 1000) целое число, уже какого-то выставленного лота, 
-        /// или можно указать любую сумму больше цены самого дешевого лота, во втором случае будет куплен предмет по самой низкой цене.
-        /// partner=[partner]&token=[token] - параметры трейд ссылки аккаунта, который получит предмет.
-        /// </summary>
-        /// <param name="id">id предмета</param>
-        /// <param name="price">Цена в копейках (1 RUB = 100)</param>
-        /// <param name="partnerLink"></param>
-        /// <returns></returns>
-        public async Task<HttpResponseMessage> BuyFor(int idItem, string price, string partner)
-        {
-            HttpClient client = new HttpClient();
-            var dict = new Dictionary<string, string>();
-            //Секретный ключ cs go market
-            dict.Add("key",_marketSecret);
-            //id предмета
-            dict.Add("id",idItem.ToString());
-            //Цена
-            dict.Add("price",price);
-            //Кому передать [partner=]&[token=]
-            dict.Add("partner",partner);
-            return await client.PostAsync(MARKETURL + "buy-for", new FormUrlEncodedContent(dict));
-        }
-        public async Task<HttpResponseMessage> BuyFor(string price, string partner, string itemHashName)
-        {
-            HttpClient client = new HttpClient();
-            var dict = new Dictionary<string, string>();
-            //Секретный ключ cs go market
-            dict.Add("key", _marketSecret);
-            //Цена
-            dict.Add("price", price);
-            //Кому передать [partner=]&[token=]
-            dict.Add("partner", partner);
-            //Название предмета
-            dict.Add("market_hash_name", itemHashName);
-            return await client.PostAsync(MARKETURL + "buy-for", new FormUrlEncodedContent(dict));
-        }
+        
     }
 }
