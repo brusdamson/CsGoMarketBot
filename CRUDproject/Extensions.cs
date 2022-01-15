ea9f17af884c404a28494f0dfce4bcfdd423aab2
Админ <@DESKTOP-71QEAO3>
2022-01-15 23:17:11 +0300

    Удалены зависимости MongoDB

--- CRUDproject/Extensions.cs
+++ CRUDproject/Extensions.cs
@@ -3,29 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Mongo.Core;
 using MongoDB.Driver;
 
 namespace CRUDproject
 {
     public static class Extensions
     {
-        public static T LoadRecordsByName<T>(this MongoCRUD mongo, string itemName, string table, IMongoDatabase db)
-        {
-            var collection = db.GetCollection<T>(table);
-            var filter = Builders<T>.Filter.Eq("ItemTitle",itemName);
-            try
-            {
-                return collection.Find(filter).First();
-            }
-            catch (Exception)
-            {
 
-                return default(T);//
-            }
-           
-
-        }
         
     }
 }
