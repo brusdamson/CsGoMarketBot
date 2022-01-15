cf901691e746215d2cb05d818f668d902d85c7ed
Админ <@DESKTOP-71QEAO3>
2022-01-15 19:50:28 +0300

    приём данных для buyFor

--- CsGoMarketBot/Program.cs
+++ CsGoMarketBot/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+
 
 namespace CsGoMarketBot
 {
@@ -10,6 +14,25 @@ namespace CsGoMarketBot
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Console.WriteLine("Введите id предмета: ");
+                var itemId = Console.ReadLine();
+                Console.WriteLine("Введите цену предмета: ");
+                var iPrice = Console.ReadLine();
+                Console.WriteLine("Введите трейд-ссылку партнера: ");
+                var strPartnerAndToken = Console.ReadLine();
+
+                
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
 
         }
     }
